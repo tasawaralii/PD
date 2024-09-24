43dc5758ef6e1916a8c6a4abd32922a12e4c57d3
⚡𝘽𝙖𝙗𝙮 ✘ 𝘽𝙤𝙨𝙨 ™ <141174860+tasawarxbaby@users.noreply.github.com>
2024-09-24 12:33:51 +0500

    Update task10.cpp
    Convert 5 Lines to 50

--- Week03/task10.cpp
+++ Week03/task10.cpp
@@ -4,16 +4,56 @@ using namespace std;
 
 main () {
 
-	int nums[15],sum,mul,sub,total;
+	int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15,sum,mul,sub,total;
 
-	for (int i = 0;i < 15;i++) {
-		cout << "Enter number " << i+1 << ": ";
-		cin >> nums[i];
-	}
+	cout << "Enter number " << 1 << ": ";
+	cin >> n1;
 	
-	sum = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
-	mul = nums[5] * nums[6] * nums[7] * nums[8] * nums[9];
-	sub = nums[10] - nums[11] - nums[12] - nums[13] - nums[14];
+	cout << "Enter number " << 1 << ": ";
+	cin >> n2;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n3;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n4;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n5;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n6;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n7;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n8;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n9;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n10;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n11;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n12;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n13;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n14;
+	
+	cout << "Enter number " << 1 << ": ";
+	cin >> n15;
+
+	sum = n1 + n2 + n3 + n4 + n5;
+	mul = n6 * n7 * n8 * n9 * n10;
+	sub = n11 - n12 - n13 - n14 - n15;
 
 	total = sum + mul - sub;
 
